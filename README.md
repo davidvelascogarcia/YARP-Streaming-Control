280281a8aac539c86db023e91fd0699a667cfd19
davidvelascogarcia <davidvelascogarcia@gmail.com>
2018-07-02 12:27:49 +0200

    Añadido programa y readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# YARP: Streaming Control
+
+- [Introducción](#introducción)
+- [Enlaces de interés](#enlaces-de-interés)
+
+## Introduccción
+
+YARP Streaming Control. Controlador multipropósito vía YARP.
+
+## Enlaces de interés
+
+[Programa](./programs)
---
+++ programs/YARP_Streaming_Control_Receptor/main.cpp
@@ -0,0 +1,88 @@
+//****************************************************
+//****************************************************
+//
+//              YARP: Streaming Control
+//
+//              Tipo: Receptor
+//
+//              Autor: David Velasco García
+//		       @davidvelascogarcia
+//
+//****************************************************
+//****************************************************
+
+// Librerias
+
+#include <iostream>
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <opencv2/opencv.hpp>
+#include <opencv/cv.h>
+#include <opencv/cvaux.h>
+#include <opencv/highgui.h>
+
+
+// Espacios de nombres
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::sig::draw;
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout<< "Bienvenido al controlador de streaming vía YARP, modo receptor"<<endl;
+    cout<<endl;
+    cout<<endl;
+    Network yarp;
+    cout<<"Iniciando red de YARP"<<endl;
+    Port puerto_receptor;
+    puerto_receptor.open("/receptor_control");
+    cout<<"Esperando órdenes..."<<endl;	
+
+    while (true) {
+
+        Bottle dato;
+	puerto_receptor.read(dato);
+        string datos=dato.toString();
+	if(datos=="a"){
+	cout<<"Moviendo hacia adelante"<<endl;
+	}
+	if(datos=="b"){
+	cout<<"Moviendo hacia atrás"<<endl;
+	}
+	if(datos=="c"){
+	cout<<"Moviendo hacia la derecha"<<endl;
+	}
+	if(datos=="d"){
+	cout<<"Moviendo hacia la izquierda"<<endl;
+	}
+	if(datos=="e"){
+	cout<<"Girando hacia la derecha"<<endl;
+	}
+	if(datos=="f"){
+	cout<<"Girando hacia la izquierda"<<endl;
+	}
+	if(datos=="g"){
+	cout<<"Mirando hacia la derecha"<<endl;
+	}
+	if(datos=="h"){
+	cout<<"Mirando hacia la izquierda"<<endl;
+	}
+	if(datos=="i"){
+	cout<<"Mirando hacia arriba"<<endl;
+	}
+	if(datos=="j"){
+	cout<<"Mirando hacia abajo"<<endl;
+	}
+	if(datos=="k"){
+	cout<<"Activando modo B"<<endl;
+	}
+	if(datos=="l"){
+	cout<<"Desconectando..."<<endl;
+	}
+   }
+    return 0;
+}
+
